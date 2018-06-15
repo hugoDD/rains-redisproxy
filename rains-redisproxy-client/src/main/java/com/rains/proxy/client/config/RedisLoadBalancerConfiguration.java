c4abe2b97e88cb4c951150d2feb045d353f879e4
hugoDD <dou@305>
2018-06-15 11:12:34 +0800

    demo

--- rains-redisproxy-client/src/main/java/com/rains/proxy/client/config/RedisLoadBalancerConfiguration.java
+++ rains-redisproxy-client/src/main/java/com/rains/proxy/client/config/RedisLoadBalancerConfiguration.java
@@ -1,7 +1,10 @@
 package com.rains.proxy.client.config;
 
+import com.netflix.discovery.EurekaClientConfig;
 import com.rains.proxy.client.redis.data.RedisProxyConnectionFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.cloud.netflix.ribbon.SpringClientFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 
 @Configuration
+@ConditionalOnClass(EurekaClientConfig.class)
+@ConditionalOnProperty(value = "eureka.client.service-url.defaultZone", matchIfMissing = false)
 public class RedisLoadBalancerConfiguration{
     @Bean
     @Primary
